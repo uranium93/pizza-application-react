7c1e8a840ecc841563db26a4ea436a3e571f795f
uranium93 <19hanafi93@gmail.com>
2019-05-29 03:35:42 +0100

    create the toolbar and the navigation

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -2,11 +2,24 @@ import React from 'react'
 import Aux from '../../hoc/Auxiliary'
 import styles from './Layout.module.css'
 import Logo from '../Logo/Logo'
+import Toolbar from '../Toolbar/Toolbar'
 
 const layout = (props)=>(
 	<Aux>
-	<Logo />
-		<div> Toolbar , sidedraw , backdrop </div>
+	
+		<Toolbar>
+		<Logo />
+		<div>
+		<ul>
+			<li><a href="/" className={styles.active}>Link 1 </a></li>
+			<li><a href="/">Link 2 </a></li>
+		</ul>
+		
+		
+		</div>
+
+
+		</Toolbar>
 		<main className={styles.content}>
 		{props.children}
 		</main>
---
+++ src/Components/Toolbar/Toolbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Toolbar.module.css'
+
+const toolbar =(props)=>{
+	return (
+<div className={styles.toolbar}>
+		{props.children}
+	</div>
+		)
+	
+}
+export default toolbar
